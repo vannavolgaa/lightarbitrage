0d95159d836e3c4aad29e31053335d2c2b095ce2
vannavolgaa <marquesasaribitrage@gmail.com>
2025-05-29 00:07:01 +0200

    add payoff engine

--- src/pybindings/core/frameworks/riskneutralpricing/py_rnp.h
+++ src/pybindings/core/frameworks/riskneutralpricing/py_rnp.h
@@ -5,6 +5,7 @@
 #include "py_blackscholes.h"
 #include "py_heston.h"
 #include "py_lbr.h"
+#include "py_payoff.h"
 
 namespace py = pybind11;
 
@@ -13,9 +14,11 @@ void PyRNP(py::module &m) {
     py::module bs = m.def_submodule("blackscholes", "Black-Scholes framework for risk-neutral pricing.");
     py::module heston = m.def_submodule("heston", "Heston model for risk-neutral pricing.");
     py::module lbr = m.def_submodule("letsberational", "LetsBeRational framework for implied volatility calculation.");
+    py::module payoff = m.def_submodule("payoff", "Payoff engine for risk-neutral pricing.");
 
     PyBlackScholes(bs);
     PyHeston(heston);
     PyLBR(lbr);
+    PyPayoff(payoff);
     
 }
