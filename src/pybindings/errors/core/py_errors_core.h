6f56248932134c978fe28705d402ec6e1e6907d2
vannavolgaa <marquesasaribitrage@gmail.com>
2025-05-28 22:40:56 +0200

    correction

--- src/pybindings/errors/core/py_errors_core.h
+++ src/pybindings/errors/core/py_errors_core.h
@@ -12,14 +12,7 @@ void PyErrorsCore(py::module_& m)
 {
     py::module emath = m.def_submodule("math", "Math errors");
     py::module eframeworks = m.def_submodule("frameworks", "Frameworks errors");
-    py::module edts = m.def_submodule("datastructures", "Datastructure errors");
-    py::module eassetpricer = m.def_submodule("assetpricer", "Asset pricer errors");
-    py::module ecalibration = m.def_submodule("calibration", "Calibration errors");
-    
     PyErrorsFrameworks(eframeworks);
     PyErrorsMath(emath);
-    PyErrorsDataStructure(edts);
-    PyErrorsAssetPricer(eassetpricer);
-    PyErrorsCalibration(ecalibration);
     
 }
